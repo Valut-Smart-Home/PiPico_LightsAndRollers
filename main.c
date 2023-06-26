7a20ca28ff4097d67dfade3c266b835edd421570
valutworkshop <valutworkshop@gmail.com>
2023-06-26 21:04:35 +0000

    _pwm_init refactor

--- main.c
+++ main.c
@@ -87,58 +87,20 @@ int main ()
 void _pwm_init()
 {
     // USING GPIO6-GPIO21
-    gpio_set_function(6, GPIO_FUNC_PWM);
-    gpio_set_function(7, GPIO_FUNC_PWM);
-    gpio_set_function(8, GPIO_FUNC_PWM);
-    gpio_set_function(9, GPIO_FUNC_PWM);
-    gpio_set_function(10, GPIO_FUNC_PWM);
-    gpio_set_function(11, GPIO_FUNC_PWM);
-    gpio_set_function(12, GPIO_FUNC_PWM);
-    gpio_set_function(13, GPIO_FUNC_PWM);
-    gpio_set_function(14, GPIO_FUNC_PWM);
-    gpio_set_function(15, GPIO_FUNC_PWM);
-    gpio_set_function(16, GPIO_FUNC_PWM);
-    gpio_set_function(17, GPIO_FUNC_PWM);
-    gpio_set_function(18, GPIO_FUNC_PWM);
-    gpio_set_function(19, GPIO_FUNC_PWM);
-    gpio_set_function(20, GPIO_FUNC_PWM);
-    gpio_set_function(21, GPIO_FUNC_PWM);
+    for (uint gpio = 6; gpio <= 21; gpio++)
+        gpio_set_function(gpio, GPIO_FUNC_PWM);
 
-    //Frequency 1kHz, Range 0-50000, Precision = 0,002 %
-    pwm_set_wrap(0, 49999);
-    pwm_set_wrap(1, 49999);
-    pwm_set_wrap(2, 49999);
-    pwm_set_wrap(3, 49999);
-    pwm_set_wrap(4, 49999);
-    pwm_set_wrap(5, 49999);
-    pwm_set_wrap(6, 49999);
-    pwm_set_wrap(7, 49999);
-    pwm_set_clkdiv_int_frac(0, 2, 8);
-    pwm_set_clkdiv_int_frac(1, 2, 8);
-    pwm_set_clkdiv_int_frac(2, 2, 8);
-    pwm_set_clkdiv_int_frac(3, 2, 8);
-    pwm_set_clkdiv_int_frac(4, 2, 8);
-    pwm_set_clkdiv_int_frac(5, 2, 8);
-    pwm_set_clkdiv_int_frac(6, 2, 8);
-    pwm_set_clkdiv_int_frac(7, 2, 8);
+    // Frequency 1kHz, Range 0-50000, Precision = 0,002 %
+    for (uint slice_num = 0; slice_num < 8; slice_num++)
+    {
+        pwm_set_wrap(slice_num, 49999);
+        pwm_set_clkdiv_int_frac(slice_num, 2, 8);
 
-    pwm_set_chan_level(0, 0, 0);
-    pwm_set_chan_level(0, 1, 0);
-    pwm_set_chan_level(1, 0, 0);
-    pwm_set_chan_level(1, 1, 0);
-    pwm_set_chan_level(2, 0, 0);
-    pwm_set_chan_level(2, 1, 0);
-    pwm_set_chan_level(3, 0, 0);
-    pwm_set_chan_level(3, 1, 0);
-    pwm_set_chan_level(4, 0, 0);
-    pwm_set_chan_level(4, 1, 0);
-    pwm_set_chan_level(5, 0, 0);
-    pwm_set_chan_level(5, 1, 0);
-    pwm_set_chan_level(6, 0, 0);
-    pwm_set_chan_level(6, 1, 0);
-    pwm_set_chan_level(7, 0, 0);
-    pwm_set_chan_level(7, 1, 0);
+        pwm_set_chan_level(slice_num, 0, 0);
+        pwm_set_chan_level(slice_num, 1, 0);
+    }
 
+    // Phase shifting
     pwm_set_enabled(0, true);
     busy_wait_at_least_cycles(15275);
     pwm_set_enabled(5, true);
